5aaf1336885302902333afcc18460a4fcdb57415
Stanley Shen (沈佑軒) <stanleyshen@gorilla-technology.com>
2022-10-26 12:51:54 +0800

    feat: add logic without unit test

--- samples/dotnet-code-coverage/src/sample/PrimeService.cs
+++ samples/dotnet-code-coverage/src/sample/PrimeService.cs
@@ -4,8 +4,17 @@ public class PrimeService
 {
     public bool IsPrime(int candidate)
     {
+        if (candidate < 2)
+        {
+            return false;
+        }
+        else if (candidate % 2 == 0 && candidate != 2)
+        {
+            return false;
+        }
+
         var root = Math.Sqrt(candidate);
-        for (int divisor = 2; divisor <= root; ++divisor)
+        for (int divisor = 3; divisor <= root; divisor += 2)
         {
             if (candidate % divisor == 0)
             {
